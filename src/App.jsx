dbb9f1363ad3cbf9cc817ef2de0907a986a0fcb2
ankush0710 <ankushkurvey053@gmail.com>
2026-04-13 18:10:08 +0530

    add another component for diplaying answer

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,58 @@
-import { useState } from 'react'
-import {URL} from  './constant'
+import { useState } from "react";
+import { URL } from "./constant";
 
 function App() {
-  const [question, setQuestion] = useState('');
+  const [question, setQuestion] = useState("");
   const [result, setResult] = useState(undefined);
 
   let payload = {
-    "contents": [{
-    "parts": [{"text": question}]
-      }]
+    contents: [
+      {
+        parts: [{ text: question }],
+      },
+    ],
   };
 
-  const handleAskQuestion = async() =>{
+  const handleAskQuestion = async () => {
     let response = await fetch(URL, {
-      method:"POST",
+      method: "POST",
       body: JSON.stringify(payload),
-    })
+    });
 
     response = await response.json();
+    const displayAnswer = response.candidates[0].content.parts[0].text;
+    displayAnswer.split("* ").map((item)=>item.trim());
     // console.log(response.candidates[0].content.parts[0].text);
-    setResult(response.candidates[0].content.parts[0].text);
-    
-  }
+    setResult(displayAnswer);
+  };
 
   return (
     <>
-    <div className='grid grid-cols-5 h-screen text-center'>
-      <div className='col-span-1 bg-zinc-400'>
-
-      </div>
-      {/* main container consist answers and input box for question  */}
-      <div className='col-span-4 p-10'>
-        <div id="constainer" className='h-120 lg:h-135'>
+      <div className="grid grid-cols-5 h-screen text-center">
+        <div className="col-span-1 bg-zinc-400"></div>
+        {/* main container consist answers and input box for question  */}
+        <div className="col-span-4 p-10">
+          <div id="constainer" className="h-120 lg:h-135">
             <div className="text-white">
-           {result}
-         </div>
-        </div>
-        <div className="flex mx-auto w-1/2 h-16 bg-zinc-800 p-1 pr-5 text-white rounded-4xl border border-zinc-700">
-          <input onChange={(e)=>setQuestion(e.target.value)} value={question} type="text" className="w-full h-full p-3 outline-none" placeholder='Ask me anything'/>
-          <button onClick={() => handleAskQuestion()} className='text-lg p-2'>Ask</button>
+              
+            </div>
+          </div>
+          <div className="flex mx-auto w-1/2 h-16 bg-zinc-800 p-1 pr-5 text-white rounded-4xl border border-zinc-700">
+            <input
+              onChange={(e) => setQuestion(e.target.value)}
+              value={question}
+              type="text"
+              className="w-full h-full p-3 outline-none"
+              placeholder="Ask me anything"
+            />
+            <button onClick={() => handleAskQuestion()} className="text-lg p-2">
+              Ask
+            </button>
+          </div>
         </div>
       </div>
-    </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/constant.js
+++ src/constant.js
@@ -1,4 +1,4 @@
-export const URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent?key=AIzaSyBIPLPbdAS6Gs_Ewcz4cK2qj4Oqbegmg0M" 
+export const URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent?key=AIzaSyCVODfB4QqobYTvTzXmBoDclpL12MP-AsU" 
 
 
 // curl "https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent" \
